ef87e6bb5f78280e4823d6fad764958cc4320a05
jolusca1 <joaolucasbarroso2000@gmail.com>
2025-02-18 21:54:21 -0300

    fix: deploy

--- tebasbot.py
+++ tebasbot.py
@@ -30,7 +30,7 @@ async def score(interaction: discord.Interaction):
     points = await check_points(interaction.user)
     await interaction.response.send_message(f"<@{interaction.user.id}> possui {points} pontos!")
     
-# Rank
+# Ranking de usuários com mention
 @tree.command(name="ranking", description="Exibe o ranking dos usuários com mais pontos")
 async def ranking(interaction: discord.Interaction):
     ranking_list = await getRanking()  # Obtém o ranking
