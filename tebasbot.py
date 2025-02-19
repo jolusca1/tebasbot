73ba4747c81bc4f8dcb38b298df7eeae6646413a
jolusca1 <joaolucasbarroso2000@gmail.com>
2025-02-18 23:34:25 -0300

    fix: ajuste

--- tebasbot.py
+++ tebasbot.py
@@ -126,6 +126,6 @@ async def comandos(interaction: discord.Interaction):
 - Se o comando pedir um valor (ex: nome do jogo), digite conforme solicitado.  
 - No caso de menção a outro usuário, use `@` e selecione o usuário.""" 
 
-await interaction.response.send_message(command_list, ephemeral=True)
+    await interaction.response.send_message(command_list, ephemeral=True)
     
 acliente.run(os.getenv("DISCORD_TOKEN"))
