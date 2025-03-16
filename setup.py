f12bade95e92fb901ed51e034e98c55c628e0dd6
josluca1 <joaolucasbarroso2000@gmail.com>
2025-03-16 00:23:42 -0300

    fix: ajustando arquivos no servidor

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
     install_requires=[
         'discord.py',
         'python-dotenv',
