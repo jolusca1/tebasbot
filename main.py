1da7a7eb26496fbf4044dea9da84890eac8ba48c
josluca1 <joaolucasbarroso2000@gmail.com>
2025-03-16 00:17:34 -0300

    doc: documenting

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from src.presentation.bot import run_bot
+import sys
+import os
+
+# Adiciona o diretório raiz ao PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     run_bot()
